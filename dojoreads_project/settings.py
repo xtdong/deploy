2a14dc4fb485923db34ee2a4743f938b1400fc97
xtdong <davidzhao@david-linc.local>
2019-05-29 10:13:50 -0700

    django heroku

--- dojoreads_project/settings.py
+++ dojoreads_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+django_heroku.settings(locals())
